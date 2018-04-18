8a7a406aef4820fd15516cfa5c990835b3a03b4b
artfin <artfin@mail.ru>
2018-04-18 23:20:04 +0300

    added hep danila

---
+++ hep_danila/hep-mc_spectrum/main/ar_he_pes_der.h
@@ -0,0 +1,6 @@
+#ifndef AR_HE_PES_DER_H
+#define AR_HE_PES_DER_H  
+
+double ar_he_pot_der(double R);
+#endif
+
---
+++ hep_danila/hep-mc_spectrum/main/diatomic_equations_2D.h
@@ -0,0 +1,5 @@
+#ifndef DIATOMIC_EQUATIONS_2D_H
+#define DIATOMIC_EQUATIONS_2D_H  
+
+void rhs_2D(double* out, double R, double pR, double psi, double ppsi );
+#endif
